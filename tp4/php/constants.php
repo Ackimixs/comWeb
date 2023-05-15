d792cbe01c02a499e28428b7e3059ecf066d24c9
ackimixs <ackimixs@protonmail.com>
2023-05-15 19:36:01 +0200

    tp4

---
+++ tp4/php/constants.php
@@ -0,0 +1,8 @@
+<?php
+  // Database constants.
+  define('DB_USER', 'postgres');
+  define('DB_PASSWORD', '1475930862');
+  define('DB_NAME', 'tweetsdb');
+  define('DB_SERVER', 'localhost');
+  define('DB_PORT', '5432');
+?>
